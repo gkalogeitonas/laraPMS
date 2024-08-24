5f80d6fc3f356732058cb5a7d4abaf5166e845f9
Giorgos Kalogeitonas <gkalog2@gmail.com>
2024-08-24 16:37:43 +0300

    chore: Update AuthenticatedLayout.vue to include flex justify-between in header

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -49,7 +49,7 @@ const showingNavigationDropdown = ref(false);
         <!-- Main Content -->
         <div class="flex-1">
             <header class="bg-white shadow" v-if="$slots.header">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between">
                     <slot name="header" />
                 </div>
             </header>
