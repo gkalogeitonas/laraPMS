fcf692bb09072889837a018966a0fdfe549716d6
Giorgos Kalogeitonas <gkalog2@gmail.com>
2024-09-07 15:39:03 +0300

    chore: Update rooms index view to use RoomCard component
    - Replace the table structure with an unordered list in the rooms index view
    - Each room is now rendered using the RoomCard component
    - Improves code readability and maintainability

--- resources/js/Pages/Rooms/Index.vue
+++ resources/js/Pages/Rooms/Index.vue
@@ -6,6 +6,7 @@ import { Head } from '@inertiajs/vue3';
 import DangerButton from '@/Components/DangerButton.vue';
 import NavLink from '@/Components/NavLink.vue';
 import EditButton from '@/Components/EditButton.vue';
+import RoomCard from '@/Pages/Rooms/RoomCard.vue';
 
 // Assuming rooms are passed as a prop
 const props = defineProps({
@@ -20,51 +21,16 @@ const props = defineProps({
     <AuthenticatedLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">My Rooms</h2>
-            <a href="/rooms/create" class="text-green-600 hover:text-green-900">Create New Room</a>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                            <tr>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Room Name
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Type
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Status
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Actions
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            <tr v-for="room in props.rooms" :key="room.id">
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    <NavLink :href="route('rooms.show', room.id)"> {{ room.name }}</NavLink>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    {{ room.type }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    {{ room.status }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                    <EditButton :href="route('rooms.edit', room.id)">
-                                      Edit
-                                    </EditButton>
-                                    <DangerButton as="button" :href="route('rooms.destroy', room.id)" method="delete">
-                                       Delete
-                                    </DangerButton>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <ul>
+                        <li v-for="room in rooms" :key="room.id">
+                            <RoomCard :room="room" />
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
