81a784b2dd6952c53b51aaf0eb81bc475b3a9115
Giorgos Kalogeitonas <gkalog2@gmail.com>
2024-09-19 13:57:52 +0300

    Remove unnecessary comments from web.php routes file

--- routes/web.php
+++ routes/web.php
@@ -66,13 +66,6 @@ Route::middleware('auth')->group(function () {
 
     Route::post('/switch-tenant', [TenantSwitchController::class, 'switch']);
 
-    // Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
-    // Route::get('/customers/{customer}', [CustomerController::class, 'show'])->name('customers.show');
-    // Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
-    // Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
-    // Route::get('/customers/{customer}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
-    // Route::patch('/customers/{customer}', [CustomerController::class, 'update'])->name('customers.update');
-    // Route::delete('/customers/{customer}', [CustomerController::class, 'destroy'])->name('customers.destroy');
     Route::resource('customers', CustomerController::class);
 
 
