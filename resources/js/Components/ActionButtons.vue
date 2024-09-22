157919a8044b9552130e159135b31472c2188c4e
Giorgos Kalogeitonas <gkalog2@gmail.com>
2024-09-22 12:07:05 +0300

    Refactor ActionButtons component to include icons for View, Edit, and Delete buttons

--- resources/js/Components/ActionButtons.vue
+++ resources/js/Components/ActionButtons.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex justify-end space-x-4 mt-4">
     <NavLink v-if="showUrl" :href="showUrl"   class=" items-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150">
-        View
+        <EyeIcon class="w-5 h-5" />
     </NavLink>
     <EditButton v-if="editUrl" :href="editUrl">
-      Edit
+        <PencilIcon class="w-5 h-5" />
     </EditButton>
     <DeleteButton v-if="deleteUrl" :href="deleteUrl">
-      Delete
+        <TrashIcon class="w-5 h-5" />
     </DeleteButton>
   </div>
 </template>
@@ -17,6 +17,8 @@ import { defineProps } from 'vue';
 import NavLink from '@/Components/NavLink.vue';
 import EditButton from '@/Components/EditButton.vue';
 import DeleteButton from '@/Components/DeleteButton.vue';
+import { EyeIcon, PencilIcon, TrashIcon } from '@heroicons/vue/24/solid'
+
 
 const props = defineProps({
   editUrl: {
