9c325818430f9eedc065ed3768e645f1fa20c0d6
Giorgos Kalogeitonas <gkalog2@gmail.com>
2024-10-05 22:57:16 +0300

    Add TenantScope to BookingSource model

--- app/Models/BookingSource.php
+++ app/Models/BookingSource.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Scopes\TenantScope;
 class BookingSource extends Model
 {
     use HasFactory;
     protected $fillable = ['tenant_id', 'name'];
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new TenantScope());
+    }
 }
---
+++ tests/Unit/Models/BookingSourceTest.php
@@ -0,0 +1,69 @@
+<?php
+
+use App\Models\BookingSource;
+use App\Models\Tenant;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+
+
+uses(RefreshDatabase::class);
+
+
+test('each users have access only to it own actives tenants booking sources', function () {
+    //create two tenants
+    $tenant1 = Tenant::factory()->create();
+    $tenant2 = Tenant::factory()->create();
+    //create a users for each tenant
+    $user1 = User::factory()->create();
+    $user2 = User::factory()->create();
+    //assign the users to the tenants
+    $tenant1->users()->attach($user1);
+    $tenant2->users()->attach($user2);
+    //tennant on will have 2 booking sources
+    BookingSource::factory()->count(2)->create([
+        'tenant_id' => $tenant1->id
+    ]);
+    //tenant two will have 3 booking sources
+    BookingSource::factory()->count(3)->create([
+        'tenant_id' => $tenant2->id
+    ]);
+    $this->actingAs($user1);
+    $user1->setActiveTenant($tenant1);
+
+    //get the booking sources
+    $bookingSources = BookingSource::all();
+    //assert that the user has access to only 2 booking sources
+    expect($bookingSources->count())->toBe(2);
+    //assert that the user has access to only booking sources that belongs to tenant 1
+    $bookingSources->each(function ($bookingSource) use ($tenant1) {
+        expect($bookingSource->tenant_id)->toBe($tenant1->id);
+    });
+});
+
+test('a user without active tenant cannot see a list of booking sources', function () {
+    //create two tenants
+    $tenant1 = Tenant::factory()->create();
+    $tenant2 = Tenant::factory()->create();
+    //create a users for each tenant
+    $user1 = User::factory()->create();
+    $user2 = User::factory()->create();
+    //assign the users to the tenants
+    $tenant1->users()->attach($user1);
+    $tenant2->users()->attach($user2);
+    //tennant on will have 2 booking sources
+    BookingSource::factory()->count(2)->create([
+        'tenant_id' => $tenant1->id
+    ]);
+    //tenant two will have 3 booking sources
+    BookingSource::factory()->count(3)->create([
+        'tenant_id' => $tenant2->id
+    ]);
+    $this->actingAs($user1);
+
+    //get the booking sources
+    $bookingSources = BookingSource::all();
+    //assert that the user has access to only 2 booking sources
+    expect($bookingSources->count())->toBe(0);
+
+});
